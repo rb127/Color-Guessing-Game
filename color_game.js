9a6297b7b971132094e3b44cc5ab2b17c1a37cde
Rohit Bansal <rohitbansal0602@yahoo.com>
2019-08-28 11:51:11 +0530

    Easy mode and hard mode now working properly

--- color_game.js
+++ color_game.js
@@ -1,6 +1,6 @@
 
-var colors = makeRandomColors(6);
-
+var numOfSquares = 6;
+var colors = makeRandomColors(numOfSquares);
 var squares = document.querySelectorAll(".square");
 var right_color = pickRandColor();
 var color_disp = document.getElementById("colorDisp");
@@ -8,23 +8,65 @@ color_disp.textContent = right_color;
 var message = document.getElementById("message");
 var h1 = document.querySelector("h1");
 var reset = document.getElementById("reset");
+var easy = document.getElementById("easy");
+var hard = document.getElementById("hard");
 
-//start game for first time
-newGame();
+//easy mode
+easy.addEventListener("click", function(){
+  hard.classList.remove("selected");
+  easy.classList.add("selected");
+//creates 3 color squares and resets game
+numOfSquares = 3;
+colors = makeRandomColors(numOfSquares);
+right_color = pickRandColor();
+color_disp.textContent = right_color;
+reset.textContent = "New Colors";
+h1.style.backgroundColor = "#232323";
+message.textContent = "";
+for(var i = 0; i < squares.length; i++){
+  if(i < 3){
+    squares[i].style.backgroundColor = colors[i];
+  }
+  else{
+    squares[i].style.display = "none";
+  }
+}
+});
+
+//hard mode
+hard.addEventListener("click", function(){
+  easy.classList.remove("selected");
+  hard.classList.add("selected");
+  //creates 6 color squares and resets game
+  numOfSquares = 6;
+  colors = makeRandomColors(numOfSquares);
+  right_color = pickRandColor();
+  reset.textContent = "New Colors";
+  color_disp.textContent = right_color;
+  h1.style.backgroundColor = "#232323";
+  message.textContent = "";
+  for(var i = 0; i < colors.length; i++){
+  squares[i].style.backgroundColor = colors[i];
+  squares[i].style.display = "block";
+  }
+});
 
 //New Game button
 reset.addEventListener("click", function(){
-      colors = makeRandomColors(6);
+      //need to check mode
+
+      colors = makeRandomColors(numOfSquares);
       right_color = pickRandColor();
+      reset.textContent = "New Colors";
       color_disp.textContent = right_color;
       h1.style.backgroundColor = "#232323";
       message.textContent = "";
-      newGame();
-
+      for(var i = 0; i < colors.length; i++){
+      squares[i].style.backgroundColor = colors[i];
+      }
 });
 
 //initializes new game
-function newGame(){
 for(var i = 0; i < colors.length; i++){
   //initialising squares with colors
   squares[i].style.backgroundColor = colors[i];
@@ -34,6 +76,7 @@ for(var i = 0; i < colors.length; i++){
     var picked_color = this.style.backgroundColor;
     if(picked_color === right_color){
       changeColors(picked_color);
+      reset.textContent = "Play Again?";
     }
     else{
       message.textContent = "Try Again";
@@ -41,7 +84,7 @@ for(var i = 0; i < colors.length; i++){
     }
   });
 }
-}
+
 
 //assigns a color to each square
 function changeColors(color){
